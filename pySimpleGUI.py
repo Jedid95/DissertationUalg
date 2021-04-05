b4c455f0019927aa7acf262c79e5a94197807837
Jedid95 <jedid.santos@gmail.com>
2021-04-05 22:11:12 +0100

    edit files

--- pySimpleGUI.py
+++ pySimpleGUI.py
@@ -1,18 +1,38 @@
 import PySimpleGUI as sg
+import pandas as pd
 
-sg.theme('DarkAmber')   # Add a touch of color
+file_name = "ETS_enderecos.xls"
+df = pd.read_excel(io=file_name)
+
+types_devices = []
+
+sg.theme('Reddit')
+#theme_name_list = sg.theme_list()
+#print(theme_name_list)
+
 # All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+layout = [  [sg.Text('Add device types equally to what is in the ETS file')],
+            [sg.Text('Enter with Type Device'), sg.InputText()],
+            [sg.Button('Add'), sg.Button('Finish'), sg.Button('Cancel')] ]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Menu Configuration', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Finish' or event == 'Cancel': # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
+
+    if event == 'Add':
+        confirm = sg.PopupOKCancel('Add this type？', title='Confirm')
+        if confirm == 'OK':
+            types_devices.append(values[0])
+
+    #print('You entered ', values[0])
+    #print(types_devices)
 
 window.close()
+
+print(types_devices)
+#teste = df.query('Type == "Atuadores"').head()
+#print(teste['Type'])
