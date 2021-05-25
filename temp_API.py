3e02d4b81410eccc7990a140053389399e40c165
Jedid Jah Dorneles dos Santos <57962005+Jedid95@users.noreply.github.com>
2021-05-25 22:48:10 +0100

    Delete temp_API.py

--- temp_API.py
+++
@@ -1,6 +0,0 @@
-import urllib3
-import json
-http = urllib3.PoolManager()
-r = http.request('GET', 'http://api.openweathermap.org/data/2.5/weather?q=faro&appid=0b3c1c23e3e51f2a7f11ff0c819ad7dd&units=metric')
-data =json.loads(r.data)
-print(data['main'])
